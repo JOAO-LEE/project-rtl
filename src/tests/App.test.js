fe2f98e3fd6794124a573756b43bae8e57240fb4
João Lee <joaumlimaum@gmail.com>
2022-06-21 10:45:10 -0300

    Requisito 1 concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    it('O primeiro link deve possuir o texto "Home"', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const linkToHome = screen.getByRole('link', { name: /home/i });
+      expect(linkToHome).toBeInTheDocument();
+    });
+    it('O segundo link deve possuir o texto "About"', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const linkToAbout = screen.getByRole('link', { name: /about/i });
+      expect(linkToAbout).toBeInTheDocument();
+    });
+    it('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const linkToFavorites = screen.getByRole('link',
+        { name: /favorite pokémons/i });
+      expect(linkToFavorites).toBeInTheDocument();
+});
+})
+/* acessar os elementos da tela
+interagir com os elementos (se necessário)
+fazer os tests (expects) */
