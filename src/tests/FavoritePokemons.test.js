dc6614bd5ccf73b9f68756f0c8161e1acdbc922f
João Lee <joaumlimaum@gmail.com>
2022-06-30 00:47:19 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  test('É exibido "No favorite pokemon found" caso a pessoa não tenha pokémons favoritos',
+    () => {
+      render(<FavoritePokemons />);
+      const noFavoriteWarn = screen.getByText(/no favorite pokemon found/i);
+      expect(noFavoriteWarn).toBeInTheDocument();
+    });
+});
