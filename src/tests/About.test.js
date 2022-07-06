493389975c834800a9eb1efe4a5df66032c5d274
João Lee <joaumlimaum@gmail.com>
2022-07-06 04:45:02 -0300

    Requisito 2 - Reescrevendo questões - colocando como template string

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../pages/About';
 
-describe('Teste se a página contém as informações sobre a Pokédex', () => {
-  it('Teste se a página contém um heading "h2" com o texto "About Pokédex"', () => {
+describe('2. Teste o componente <About.js />', () => {
+  test(`Teste se a página contém as informações sobre a Pokédex;
+  Teste se a página contém um heading "h2" com o texto "About Pokédex`, () => {
     render(<About />);
     const aboutHeadingText = screen.getByRole('heading',
       { name: /about pokédex/i, level: 2 });
     expect(aboutHeadingText).toBeInTheDocument();
   });
 
-  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
     render(<About />);
     const aboutParagraphs = screen.getAllByText(/pokémons/i);
     expect(aboutParagraphs).toHaveLength(2);
   });
 
-  it('Teste se a página contém a seguinte imagem de uma Pokédex: "https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png"', () => {
+  test(`Teste se a página contém a seguinte imagem de uma Pokédex: 
+  https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`, () => {
     render(<About />);
     const pokedexUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const pokedexImg = screen.getByAltText(/pokédex/i);
